49613ed85974552bc5185294f01ec3156a5a61a4
JakubKub13 <jakubkubala3@gmail.com>
2023-08-31 05:34:02 +0200

    debuggin

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
+
+const port = env.PORT;
+
+mongoose.connect(env.MONGO_CONNECTION_STRING!)
+    .then(() => {
+        console.log("Connected to the database!");
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);  
+        });
+    })
+    .catch(console.error);
+
